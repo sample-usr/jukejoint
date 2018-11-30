c8f6564cac0a13b758f861ce158fb20b8d7902b7
robeno <robenosk@gmail.com>
2018-11-30 23:07:56 +0100

    fixed server typing

--- jukejoint_streamer/src/controller/PlayerController.ts
+++ jukejoint_streamer/src/controller/PlayerController.ts
@@ -1,19 +1,19 @@
 import Provider from '../providers/Provider';
 import Player from '../player/Player';
 
-import { IPlayer } from '@jukejoint/common/lib/interfaces'
+import { IPlayerState } from '@jukejoint/common/lib/interfaces'
 
 import { websocketInstance } from '../services/WebsocketService';
 
 const player = Player.getInstance();
 
-const tempIPlayer:IPlayer = {
- volume: 0,
- currentSongId: "asdf",
- playlist:null
+const tempIPlayer: IPlayerState = {
+  volume: 0,
+  currentSongId: "asdf",
+  playlist: null
 }
 
-export async function addSong(ctx:any) {
+export async function addSong(ctx: any) {
   const newSong = await Provider.getSongInfo(ctx.body.songURL, ctx.body.providerType);
   if (player.add(newSong)) {
     // Redirect
@@ -22,19 +22,19 @@ export async function addSong(ctx:any) {
   }
 }
 
-export async function pauseSong(ctx:any) {
+export async function pauseSong(ctx: any) {
   // await player.pause();
   console.log('in pause');
   websocketInstance.sendMsg(tempIPlayer);
   ctx.status = 200;
 }
 
-export async function playSong(ctx:any) {
+export async function playSong(ctx: any) {
   await player.play();
   ctx.status = 200;
 }
 
-export async function getCurrentSong(ctx:any) {
+export async function getCurrentSong(ctx: any) {
   const currentSong = player.getCurrentSong();
   if (currentSong) {
     // send PlayerObject on WS
@@ -43,12 +43,12 @@ export async function getCurrentSong(ctx:any) {
   }
 }
 
-export async function increaseVolume(ctx:any) {
+export async function increaseVolume(ctx: any) {
   player.increaseVolume();
   ctx.status = 200;
 }
 
-export async function decreaseVolume(ctx:any) {
+export async function decreaseVolume(ctx: any) {
   player.decreaseVolume();
   ctx.status = 200;
 }
