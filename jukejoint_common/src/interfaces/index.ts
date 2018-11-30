f37a37d79ba3d02e40a4f9eacb70100f69991bec
robeno <robenosk@gmail.com>
2018-11-30 23:14:44 +0100

    Fixed ts stuff

--- jukejoint_common/src/interfaces/index.ts
+++ jukejoint_common/src/interfaces/index.ts
@@ -1,7 +1,9 @@
 import IProvider from './IProvider';
 import { ProvidersDTOType } from './ProviderTypes';
+import IPlayerState from './IPlayerState';
 
 export {
   IProvider,
   ProvidersDTOType,
+  IPlayerState
 }
