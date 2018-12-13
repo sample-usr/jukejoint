b900c1fc35fda5cdf01acc6e4fdf22fc3a3b07c8
robeno <robenosk@gmail.com>
2018-12-13 22:05:20 +0100

    :lipstick: Show current volume in UI

--- jukejoint_frontend/src/pod/player/components/PlayerVolume.tsx
+++ jukejoint_frontend/src/pod/player/components/PlayerVolume.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { AppContextProp, withAppContext } from '../../../toolkit/util/AppContext';
 
 import styles from '../styles/PlayerVolume.module.css';
 import stylesHelpers from '../../../assets/css/helpers.module.css';
+
 
 import { ReactComponent as IcoVolume } from '../../../assets/img/ico/ico-volume.svg';
 import { ReactComponent as IcoVolumeUp } from '../../../assets/img/ico/ico-volume-up.svg';
@@ -13,17 +15,12 @@ type State = {
   isVisible: boolean;
 }
 
-class VolumeControl extends Component<{}, State> {
+type Props = AppContextProp;
+
+class VolumeControl extends Component<Props, State> {
   readonly state: State = {
     isVisible: false
   }
-
-  // constructor(props: any) {
-  //   super(props);
-  //   this.state = {
-  //     isVisible: false
-  //   };
-  // }
 
   private toggleIsVisible = () => {
     const { isVisible } = this.state;
@@ -36,15 +33,14 @@ class VolumeControl extends Component<{}, State> {
 
   private volumeUp = () => {
     PlayerService.getInstance().increaseVolume();
-    console.log('Volume Up');
   }
   private volumeDown = () => {
     PlayerService.getInstance().decreaseVolume();
-    console.log('Volume Down');
   }
 
   public render() {
     const { isVisible } = this.state;
+    const { player } = this.props.appContext;
 
     return (
       <div className={styles.wrapper}>
@@ -53,6 +49,7 @@ class VolumeControl extends Component<{}, State> {
         </div>
 
         <div className={`${styles.control} ${isVisible ? styles.show : ''}`}>
+          <div className={styles.playerVolume}>{player ? `${player.volume} %` : ''}</div>
           <div onClick={this.volumeUp} className={stylesHelpers.pointer}>
             <IcoVolumeUp className={styles.ico} />
           </div>
@@ -65,4 +62,4 @@ class VolumeControl extends Component<{}, State> {
   }
 }
 
-export default VolumeControl;
+export default withAppContext(VolumeControl);
